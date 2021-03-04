1a5ba559be6cd38685283c398a9cd01b35a7916d
mmdrez4 <mohammadreza.dn80@gmail.com>
2021-03-04 11:47:30 +0330

    create SecondActivity.java and connect it to the MainActivity.java

--- app/src/main/java/ir/madeinlobb/hw1/MainActivity.java
+++ app/src/main/java/ir/madeinlobb/hw1/MainActivity.java
@@ -2,13 +2,68 @@ package ir.madeinlobb.hw1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button openSecondActivity;
+    Integer number;
+    TextView textView;
+    Button button;
+    EditText first;
+    EditText second;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openSecondActivity = findViewById(R.id.open_second_activity);
+
+        openSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+
+//        CountDownTimer countDownTimer = new CountDownTimer(10000,1000) {
+//            @Override
+//            public void onTick(long remainingTime) {
+//                Log.d("TAG","countDownTimer" + remainingTime);
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//                Log.d("TAG","countDownTimer onFinish");
+//            }
+//        };
+//        countDownTimer.start();
+//
+//        textView = findViewById(R.id.text);
+//        button = findViewById(R.id.buttonPanel);
+//        first = findViewById(R.id.myEditText);
+//
+//        Log.d("NUM",textView.getText().toString());
+//
+//        Toast toast = Toast.makeText(MainActivity.this,"ready",Toast.LENGTH_LONG);
+//        toast.show();
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String name = first.getText().toString();
+//                textView.setText("salam" + name);
+//            }
+//        });
     }
 }
